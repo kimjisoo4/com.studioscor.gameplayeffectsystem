94caa7525b88b3e9cf05df3a135a2a9995f3d06d
KimScor <58765606+kimjisoo4@users.noreply.github.com>
2024-04-22 16:22:57 +0900

    Add Task Action
    [ Added ]
     GameObject 를 받아서 실행되는 Task Action 추가.

---
+++ Extend/WithSerializeReference/TakeGameplayEffectsTaskAction.cs
@@ -0,0 +1,48 @@
+﻿using StudioScor.Utilities;
+using UnityEngine;
+
+namespace StudioScor.GameplayEffectSystem.Extend.Task
+{
+    public class TakeGameplayEffectsTaskAction : TaskAction
+    {
+        [Header(" [ Take Gameplay Effect Action ] ")]
+        [SerializeField] private GameplayEffect[] _gameplayEffects;
+        [SerializeReference]
+#if SCOR_ENABLE_SERIALIZEREFERENCE
+        [SerializeReferenceDropdown]
+#endif
+         private IIntegerVariable _level = new DefaultIntegerVariable();
+
+        private TakeGameplayEffectsTaskAction _original;
+
+        public override void Setup(GameObject owner)
+        {
+            base.Setup(owner);
+
+            _level.Setup(Owner);
+        }
+        public override ITaskAction Clone()
+        {
+            var clone = new TakeGameplayEffectsTaskAction();
+
+            clone._original = this;
+            clone._level = _level.Clone();
+
+            return clone;
+        }
+
+        public override void Action(GameObject target)
+        {
+            if(target.TryGetGameplayEffectSystem(out IGameplayEffectSystem gameplayEffectSystem))
+            {
+                var effects = _original is null ? _gameplayEffects : _original._gameplayEffects;
+
+                foreach (var effect in effects)
+                {
+                    gameplayEffectSystem.TryTakeEffect(effect, Owner, _level.GetValue());
+                }
+                
+            }
+        }
+    }
+}
